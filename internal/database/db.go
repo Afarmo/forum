cd276cb6a1f4523e999287ceb9cf54fccf228e54
Abderahman Afaryad <kallahti@hotmail.com>
2026-09-08 04:41:49 +0300

    Add SQLite Open() function with FK enforcement

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,1 +1,23 @@
 package database
+
+import (
+	"database/sql"
+)
+
+func Open() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "forum.db")
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	if _, err := db.Exec("PRAGMA foreign_keys = ON"); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
