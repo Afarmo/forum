32c0d1a82cf3b12b167fead4378035b8127ebb5c
Abderahman Afaryad <kallahti@hotmail.com>
2026-09-10 16:19:36 +0300

    Better naming convention

--- internal/models/category.go
+++ internal/models/category.go
@@ -1,6 +1,6 @@
 package models
 
 type Category struct {
-	Id   int    `json:"id"`
+	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
--- internal/models/post.go
+++ internal/models/post.go
@@ -4,8 +4,8 @@ import "time"
 
 type Post struct {
 	Content        string    `json:"content"`
-	PostId         int       `json:"post_id"`
-	UserId         int       `json:"user_id"`
+	PostID         int       `json:"post_id"`
+	UserID         int       `json:"user_id"`
 	PictureContent string    `json:"picture_content"`
 	CreatedAt      time.Time `json:"created_at"`
 }
