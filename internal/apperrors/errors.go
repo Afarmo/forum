98711c66c9d6de57af2b8e4b9a1272e3d3f68413
Abderahman Afaryad <kallahti@hotmail.com>
2026-09-16 23:22:52 +0300

    Add ErrInvalidCredentials

--- internal/apperrors/errors.go
+++ internal/apperrors/errors.go
@@ -6,15 +6,16 @@ import (
 )
 
 var (
-	ErrNotFound          = errors.New("record not found")
-	ErrDuplicateKey      = errors.New("duplicate key violation")
-	ErrInvalidInput      = errors.New("invalid input")
-	ErrInvalidID         = errors.New("invalid ID")
-	ErrTransactionStart  = errors.New("failed to begin transaction")
-	ErrTransactionCommit = errors.New("failed to commit transaction")
-	ErrInvalidUsername   = errors.New("invalid usernam")
-	ErrInvalidEmail      = errors.New("invalid email")
-	ErrInvalidPassword   = errors.New("invalid password")
+	ErrNotFound           = errors.New("record not found")
+	ErrDuplicateKey       = errors.New("duplicate key violation")
+	ErrInvalidInput       = errors.New("invalid input")
+	ErrInvalidID          = errors.New("invalid ID")
+	ErrTransactionStart   = errors.New("failed to begin transaction")
+	ErrTransactionCommit  = errors.New("failed to commit transaction")
+	ErrInvalidUsername    = errors.New("invalid usernam")
+	ErrInvalidEmail       = errors.New("invalid email")
+	ErrInvalidPassword    = errors.New("invalid password")
+	ErrInvalidCredentials = errors.New("invalid credentials")
 )
 
 func Log(err error) {
