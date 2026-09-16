2158dae404ecb25850edbe3f5292d8f90b9be0e3
Abderahman Afaryad <kallahti@hotmail.com>
2026-09-16 17:14:56 +0300

    Fix queries to match schema and remove tx from CreateUser() since single INSERT is atomic

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -21,72 +21,91 @@ func NewUserRepository(db *sql.DB) *UserRepository {
 }
 
 func (r *UserRepository) CreateUser(ctx context.Context, user *models.User) error {
-	tx, txErr := r.db.BeginTx(ctx, nil)
-	if txErr != nil {
-		return apperrors.ErrTransactionStart
-	}
-	defer tx.Rollback() // if there is error revert changes to the db back to before the changes
-	query := `INSERT INTO users(username, email, user_password, profile_picture) VALUES(?,?,?,?)`
+	query := `
+		INSERT INTO users(
+			username,
+			email,
+			password_hash,
+			password_salt,
+			profile_picture
+		) VALUES(?,?,?,?,?)
+	`
+
 	now := time.Now()
-	result, err := tx.ExecContext(ctx, query, user.UserName, user.Email, user.Password, user.ProfilePicture)
+	result, err := r.db.ExecContext(
+		ctx,
+		query,
+		user.UserName,
+		user.Email,
+		user.PasswordHash,
+		user.PasswordSalt,
+		user.ProfilePicture,
+	)
+
 	if err != nil {
 		if strings.Contains(err.Error(), "UNIQUE constraint failed") {
 			return apperrors.ErrDuplicateKey
 		}
 		return err
 	}
+
 	id, err := result.LastInsertId()
 	if err != nil {
 		return err
 	}
+
 	user.ID = int(id)
 	user.CreatedAt = now
-	return tx.Commit()
+
+	return nil
 }
 
 func (r *UserRepository) FindUserByEmail(ctx context.Context, email string) (*models.User, error) {
-	query := `SELECT id, username, email, user_password, profile_picture, created_at FROM USERS WHERE email = ?`
+	query := `SELECT id, username, email, password_hash, password_salt, profile_picture, created_at FROM USERS WHERE email = ?`
 	user := &models.User{}
 
 	var profilePicture sql.NullString
-	err := r.db.QueryRowContext(ctx, query, email).Scan(
+	if err := r.db.QueryRowContext(ctx, query, email).Scan(
 		&user.ID,
 		&user.UserName,
 		&user.Email,
-		&user.Password,
+		&user.PasswordHash,
+		&user.PasswordSalt,
 		&profilePicture,
 		&user.CreatedAt,
-	)
-	if err != nil {
+	); err != nil {
 		if err == sql.ErrNoRows {
 			return nil, apperrors.ErrNotFound
 		}
 		return nil, err
 	}
+
 	return user, nil
 }
 
 func (r *UserRepository) FindUserById(ctx context.Context, id int) (*models.User, error) {
-	query := `SELECT id, username, email, user_password, profile_picture, created_at FROM USERS WHERE id = ?`
+	query := `SELECT id, username, email, password_hash, password_salt, profile_picture, created_at FROM USERS WHERE id = ?`
 	user := &models.User{}
 
 	var profilePicture sql.NullString
-	err := r.db.QueryRowContext(ctx, query, id).Scan(
+	if err := r.db.QueryRowContext(ctx, query, id).Scan(
 		&user.ID,
 		&user.UserName,
 		&user.Email,
-		&user.Password,
+		&user.PasswordHash,
+		&user.PasswordSalt,
 		&profilePicture,
 		&user.CreatedAt,
-	)
-	if err != nil {
+	); err != nil {
 		if err == sql.ErrNoRows {
 			return nil, apperrors.ErrNotFound
 		}
 		return nil, err
 	}
+
 	return user, nil
 }
+
 func (r *UserRepository) UpdateProfilePicture(ctx context.Context, userID int, picturePath string) error {
 	query := `UPDATE users SET profile_picture = ? WHERE id = ?`
 	_, err := r.db.ExecContext(ctx, query, picturePath, userID)
